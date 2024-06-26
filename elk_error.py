5a6776e9fb4bc8180ea3a53fe77166b6a610c0c9
qxp0720 <62988477+qxp0720@users.noreply.github.com>
2024-06-26 17:54:36 +0800

    Update elk_error.py

--- elk_error.py
+++ elk_error.py
@@ -9,25 +9,14 @@ from elasticsearch import Elasticsearch
 es_host = ('172.16.1.121', '172.16.1.122', '172.16.1.123')
 
 nginx_dict = {
-    '市级': 'sj-nginx',
-    '市直属': 'szs-nginx',
-    '园区分中心': 'yqfzx-nginx',
-    '相城分中心': 'xcfzx-nginx',
-    '高新区分中心': 'gxfzx-nginx',
-    '昆山分中心': 'ksfzx-nginx',
-    '太仓分中心': 'tcfzx-nginx',
-    '姑苏分中心': 'gsfzx-nginx',
-    '吴中分中心': 'wzfzx-nginx',
-    '张家港分中心': 'zjgfzx-nginx',
-    '常熟分中心': 'csfzx-nginx',
-    '吴江分中心': 'wjfzx-nginx',
-    '园区易加互动': 'sip-nginx',
-    '小通优课': 'xtyk-nginx',
+    'sj': 'sj-nginx',
+    'szs': 'szs-nginx',
+
 }
 
 ###钉钉报警
 def dingding(user, text):
-    webhook = "https://oapi.dingtalk.com/robot/send?access_token=69a7a866f9c80e6217c47e3b3b79ff6869191c586ce2214b1149bcad7f9d0d4d"
+    webhook = "https://oapi.dingtalk.com/robot/send?access_token="
     data = {
         "msgtype": "text",
         "text": {
